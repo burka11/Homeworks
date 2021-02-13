3e4976e70d00d57b3e1ee7f46198013b598f602d
Burak Yıldız <developerburakyildiz@gmail.com>
2021-02-13 18:39:18 +0300

    MyDictionary ödevi tamamlandı.

---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> sehirler = new MyDictionary<string>();
+            sehirler.Add("İstanbul");
+            sehirler.Add("Ankara");
+            sehirler.Add("İzmir");
+
+            Console.WriteLine(sehirler.Count + " elemanlıdır.");
+        }
+    }
+}
